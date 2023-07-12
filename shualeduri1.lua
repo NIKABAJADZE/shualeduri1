d943fdc09c8142b8b1e737a6319ccb9adad82184
NIKABAJADZE <nikabajadze2006@gmail.com>
2023-07-12 23:56:06 +0400

    shualeduri1

---
+++ shualeduri1.lua
@@ -0,0 +1,30 @@
+function quiz(number)
+    print("     First question:\n           Which is the largest peninsula in the world?".."\n           you:Arabian peninsula")
+    answer = number
+        return answer
+end
+number = true
+print(quiz(number))
+
+
+function quiz(number)
+    print("     Second question:\n            When did the Battle of Didgori?".."\n            On August 12, 1121")
+    answer = number
+        return answer
+end
+number = true
+print(quiz(number))
+
+function quiz(number)
+    print("     The third question:\n            How Much is 730*15?\n            you:it's 10950")
+    answer = number
+        return answer
+end
+number = true
+print(quiz(number))
+
+function quiz(number)
+    print("     Fouth question:\n            What year was the Titanic built?\n            you April 12, 1915")
+end
+print(quiz(number))
+print("This answer is incorrect\nYou lose")
